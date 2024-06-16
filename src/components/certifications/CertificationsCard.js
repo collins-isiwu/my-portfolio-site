0b1ade28ee6d4ba4b3edc5107df268e86e403b49
Collins Isiwu <collinscisiwu@outlook.com>
2024-06-16 23:06:14 +0100

    chore: update

--- src/components/certifications/CertificationsCard.js
+++ src/components/certifications/CertificationsCard.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import { BsGithub } from "react-icons/bs";
-import { FaGlobe } from "react-icons/fa";
 
-const CertificationsCard = ({ title, des, src, href, git }) => {
+const CertificationsCard = ({ title, src, href }) => {
   return (
     <div className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
       <div className="w-full h-[80%] overflow-hidden rounded-lg">
